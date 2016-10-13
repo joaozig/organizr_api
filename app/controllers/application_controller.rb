3834d80eb1325c3868e853b8920b7147904fe2d5
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-13 08:58:37 -0300

    Refactoring authenticate_with_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::API
 	 def authenticate_with_token!
 	 	@authentication = Authentication.new(request.headers["Authentication"])
     render json: { errors: "Not authenticated" },
-										status: :unauthorized unless @authentication.authenticated?
+										status: :unauthorized unless user_signed_in?
+  end
+
+  def user_signed_in?
+  	@authentication.authenticated?
   end
 end
