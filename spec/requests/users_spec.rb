8f3b47e6d03db2dd6ac3343a0d5bd196e9e020a6
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-18 20:15:45 -0300

    Adds authentication to user tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,21 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Users Requests", type: :request do
 
-  describe "GET #show" do
-  	before(:each) do
-  		@user = FactoryGirl.create :user
-  		get user_path(id: @user.id), nil, { "Authorization": @user.auth_token }
-  	end
-
-    it "respond with http status 200" do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it "returns the user on a hash" do
-    	expect(json_response[:email]).to eq(@user.email)
-    end
-  end
-
   describe "POST #create" do
     context "when is successfully created" do
       before(:each) do
@@ -53,50 +38,91 @@ RSpec.describe "Users Requests", type: :request do
     end
   end
 
-  describe "PATCH #update" do
-    context "when is successfully updated" do
+  context "when not signed in" do
+    describe "GET #show" do
+      it "responds with http status 401" do
+        get user_path(1)
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    describe "PATCH #update" do
+      it "responds with http status 401" do
+        patch user_path(1)
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "responds with http status 401" do
+        delete user_path(1)
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  context "when signed in" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @authentication_header = { "Authentication": @user.auth_token }
+    end
+
+    describe "GET #show" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch user_path(id: @user.id), { user: { email: "new@email.com" } }
+        get user_path(id: @user.id), nil, @authentication_header
       end
 
       it "respond with http status 200" do
         expect(response).to have_http_status(:ok)
       end
 
-      it "returns the updated user on a hash" do
-        expect(json_response[:email]).to eq("new@email.com")
+      it "returns the user on a hash" do
+        expect(json_response[:email]).to eq(@user.email)
       end
     end
 
-    context "when is not updated" do
+    describe "PATCH #update" do
+      context "when is successfully updated" do
+        before(:each) do
+          patch user_path(id: @user.id), { user: { email: "new@email.com" } }, @authentication_header
+        end
+
+        it "respond with http status 200" do
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "returns the updated user on a hash" do
+          expect(json_response[:email]).to eq("new@email.com")
+        end
+      end
+
+      context "when is not updated" do
+        before(:each) do
+          patch user_path(id: @user.id), {user: { email: "bademail.com" } }, @authentication_header
+        end
+
+        it "respond with http status 422" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it "returns a json with errors" do
+          expect(json_response).to have_key(:errors)
+        end
+
+        it "returns why the user could not be updated" do
+          expect(json_response[:errors][:email]).to include("is invalid")
+        end
+      end
+    end
+
+    describe "DELETE #destroy" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch user_path(id: @user.id), {user: { email: "bademail.com" } }
+        delete user_path(@user.id), nil, @authentication_header
       end
 
-      it "respond with http status 422" do
-        expect(response).to have_http_status(:unprocessable_entity)
+      it "respond with http status 204" do
+        expect(response).to have_http_status(:no_content)
       end
-
-      it "returns a json with errors" do
-        expect(json_response).to have_key(:errors)
-      end
-
-      it "returns why the user could not be updated" do
-        expect(json_response[:errors][:email]).to include("is invalid")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    before(:each) do
-      @user = FactoryGirl.create :user
-      delete user_path(@user.id)
-    end
-
-    it "respond with http status 204" do
-      expect(response).to have_http_status(:no_content)
     end
   end
 end
